1666e994cade03e2486bec063ed15c15a99318c7
KeenanFernandes <keenan030900@gmail.com>
2025-09-16 18:25:54 +0400

    checkpoint

--- client/src/pages/admin/learningPathPage.tsx
+++ client/src/pages/admin/learningPathPage.tsx
@@ -483,7 +483,7 @@ const LearningPathsPage = () => {
       // Prepare data for API
       const learningPathData = {
         name: formData.name,
-        unitIds: [], // Empty array for now, can be populated later
+        unitIds: null, // Set to null by default, can be populated later
         roleCategoryId: formData.roleCategoriesId
           ? parseInt(formData.roleCategoriesId)
           : null,
